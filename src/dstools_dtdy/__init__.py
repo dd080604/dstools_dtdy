a05dc5b9398f09c0bb2a8335bfb32c0d73e82d0e
DylanDay <48579518+dd080604@users.noreply.github.com>
2026-02-23 14:15:17 -0600

    Import linear model functions and define __all__

--- src/dstools_dtdy/__init__.py
+++ src/dstools_dtdy/__init__.py
@@ -1,1 +1,19 @@
+from .linear_model import (
+    LinearModelFit,
+    mylm,
+    coef,
+    fitted_values,
+    residuals,
+    predict,
+    cv_mylm,
+)
 
+__all__ = [
+    "LinearModelFit",
+    "mylm",
+    "coef",
+    "fitted_values",
+    "residuals",
+    "predict",
+    "cv_mylm",
+]
