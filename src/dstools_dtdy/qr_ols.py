a4037acc380e2d88c45c86d43715ac331758f4af
DylanDay <48579518+dd080604@users.noreply.github.com>
2026-03-16 20:52:42 -0500

    Add my_lm_QR function for linear regression
    Implement my_lm_QR function for linear regression using QR decomposition.

---
+++ src/dstools_dtdy/qr_ols.py
@@ -0,0 +1,15 @@
+def my_lm_QR(X, y, add_intercept=True):
+
+    X = np.asarray(X)
+    y = np.asarray(y).reshape(-1)
+
+    if add_intercept:
+        X = np.column_stack((np.ones(len(X)), X))
+
+    Q, R = householder_qr(X)
+
+    Qt_y = Q.T @ y
+
+    beta = np.linalg.solve(R[:R.shape[1], :], Qt_y[:R.shape[1]])
+
+    return beta
