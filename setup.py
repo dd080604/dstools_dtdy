c5610864b76abf55a2b65cf9d7bb4abfc945948f
DylanDay <48579518+dd080604@users.noreply.github.com>
2026-03-16 20:58:48 -0500

    Add QR extension to setup.py
    Added a new C extension for QR decomposition alongside the existing linear model Cython extension.

--- setup.py
+++ setup.py
@@ -7,9 +7,17 @@ extensions = [
         name="dstools_dtdy.linear_model_cython",
         sources=["src/dstools_dtdy/linear_model_cython.pyx"],
         include_dirs=[np.get_include()],
-    )
+    ),
+    Extension(
+        name="dstools_dtdy.qr",
+        sources=["src/dstools_dtdy/qr.c"],
+        include_dirs=[np.get_include()],
+    ),
 ]
 
 setup(
-    ext_modules=cythonize(extensions, language_level="3"),
+    ext_modules=cythonize(
+        [extensions[0]],
+        language_level="3",
+    ) + [extensions[1]],
 )
