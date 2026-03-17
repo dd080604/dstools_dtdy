8f8f6245533c7542d246131eea92266562184cf1
DylanDay <48579518+dd080604@users.noreply.github.com>
2026-03-16 22:06:36 -0500

    Refactor my_lm_QR to use LinearModelFit

--- src/dstools_dtdy/qr_ols.py
+++ src/dstools_dtdy/qr_ols.py
@@ -1,17 +1,26 @@
 import numpy as np
+from .qr_wrapper import householder_qr
+from .linear_model import LinearModelFit
 
 def my_lm_QR(X, y, add_intercept=True):
+    X = np.asarray(X, dtype=float)
+    y = np.asarray(y, dtype=float).reshape(-1)
 
-    X = np.asarray(X)
-    y = np.asarray(y).reshape(-1)
+    if X.ndim == 1:
+        X = X.reshape(-1, 1)
 
     if add_intercept:
-        X = np.column_stack((np.ones(len(X)), X))
+        X = np.column_stack((np.ones(X.shape[0]), X))
 
     Q, R = householder_qr(X)
 
-    Qt_y = Q.T @ y
+    p = X.shape[1]
+    beta = np.linalg.solve(R[:p, :p], (Q.T @ y)[:p])
+    fitted = X @ beta
+    resid = y - fitted
 
-    beta = np.linalg.solve(R[:R.shape[1], :], Qt_y[:R.shape[1]])
-
-    return beta
+    return LinearModelFit(
+        coefficients=beta,
+        residuals=resid,
+        fitted_values=fitted,
+    )
